3f37869141d427d2ec3bf4731cbc01e6b91ace94
jimmery <jiangxuanzhi@gmail.com>
2016-12-06 02:14:58 -0800

    I have no idea what I'm doing. l m a o. i'll figure it out tomorrow maybe. i hope.

--- max.v
+++ max.v
@@ -1,23 +1,28 @@
 module max( input [31:0] i1, 
             input [31:0] i2,
             input en,  
+            input clk, 
             output reg [31:0] max); 
     initial begin
         max = 0;
     end
-    always @ (en) begin
-        if (i1[31]) begin
-            if (i2[31])
-                max = 0;
-            else
-                max = i2;
+    always @ (posedge clk) begin
+        if (en) begin
+            if (i1[31]) begin
+                if (i2[31])
+                    max = 0;
+                else
+                    max = i2;
+            end else begin
+                if (i2[31])
+                    max = i1;
+                else if (i1[30:0] > i2[30:0])
+                    max = i1;
+                else
+                    max = i2;
+            end
         end else begin
-            if (i2[31])
-                max = i1;
-            else if (i1[30:0] > i2[30:0])
-                max = i1;
-            else
-                max = i2;
+            max = 0;
         end
     end
 
